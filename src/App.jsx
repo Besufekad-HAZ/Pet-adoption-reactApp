ac12feb66352e75641cc65ddb5aa7857f6d694a1
Besufekad-HAZ <besurye@gmail.com>
2023-09-17 23:10:50 +0300

    Intial commit to upload the project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { createRoot } from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Link to="/">
+          <h1 className="adopt-me">Adopt Me!</h1>
+        </Link>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
