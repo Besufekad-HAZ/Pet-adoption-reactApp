fcae3198464e39c8b0b92dc6d649ca2d0823c29b
Besufekad-HAZ <besurye@gmail.com>
2024-10-01 11:22:06 +0300

    Remove and restart again

--- src/useBreedList.js
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import fetchBreedList from "./fetchQueries/fetchBreedList";
-
-export default function useBreedList(animal) {
-  const { data, status } = useQuery(["breeds", animal], fetchBreedList);
-  return [data?.breeds ?? [], status];
-}
