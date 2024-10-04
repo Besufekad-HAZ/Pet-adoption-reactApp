07662e19963bea58aef4e92ed20d2ce1fbf8f80e
Besufekad-HAZ <besurye@gmail.com>
2024-10-04 12:02:39 +0300

    Render the Pet Component in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+      const Pet = () => {
+      return React.createElement('div', {}, [
+        React.createElement('h1', {}, 'Luna'),
+        React.createElement('h2', {}, 'Dog'),
+        React.createElement('h3', {}, 'Havanese')
+      ],
+    )
+    }
+
     const App = () => {
-      return React.createElement('div', {},
+      return React.createElement('div', {}, [
         React.createElement('h1', {}, 'Adopt Her!'),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet),
+    ]
     )
     }
 
