c30b9ec3bb9047f78179088792e90d94473e5019
Besufekad-HAZ <besurye@gmail.com>
2024-10-04 14:44:16 +0300

    Update the ESLint configuration and fix some errors

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginImport from "eslint-plugin-import";
+import pluginJsxA11y from "eslint-plugin-jsx-a11y";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -14,6 +16,18 @@ export default [
       react: {
         version: "detect",
       },
+    },
+  },
+  {
+    plugins: {
+      import: pluginImport,
+      "jsx-a11y": pluginJsxA11y,
+    },
+  },
+  {
+    rules: {
+      "react/prop-types": 0,
+      "react/react-in-jsx-scope": 0,
     },
   },
 ];
--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
 import React from "react";
-import { createRoot } from "react-dom";
+import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import Pet from "./Pet";
 
+const x = 5;
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Her!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Wuro",
-      animal: "Cat",
-      breed: "Tiger",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="havenese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mixed" />
+    </div>
+  );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(React.createElement(App));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 Pet.propTypes = {
   name: PropTypes.string.isRequired,
