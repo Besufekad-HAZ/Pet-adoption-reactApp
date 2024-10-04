63282c243fdc12559dfba1bebedeec129aaf1b0c
Besufekad-HAZ <besurye@gmail.com>
2024-10-04 13:55:15 +0300

    Add Vite build commands and import react

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom";
+import PropTypes from "prop-types";
+
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
@@ -28,5 +32,11 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
+
+Pet.propTypes = {
+  name: PropTypes.string.isRequired,
+  animal: PropTypes.string.isRequired,
+  breed: PropTypes.string.isRequired,
+};
