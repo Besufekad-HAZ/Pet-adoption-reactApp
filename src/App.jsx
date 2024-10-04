d43438912172dbb0dba5575676bed839a2eb17ae
Besufekad-HAZ <besurye@gmail.com>
2024-10-04 14:15:17 +0300

    Add Pet props validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom";
 import PropTypes from "prop-types";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Pet = () => {
-  return <div>Pet</div>;
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+Pet.propTypes = {
+  name: PropTypes.string.isRequired,
+  animal: PropTypes.string.isRequired,
+  breed: PropTypes.string.isRequired,
 };
 
 export default Pet;
