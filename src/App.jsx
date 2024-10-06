8dd7a127baa971a095ae489b8f9c10f920ce1f38
Besufekad-HAZ <besurye@gmail.com>
2024-10-06 19:51:24 +0300

    Fix linter errors and implement the useEffect to call on using submit handler in the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import PropTypes from "prop-types";
 import SearchParams from "./SearchParams";
 
 const App = () => {
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -19,13 +19,18 @@ const SearchParams = () => {
       `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`,
     );
     const json = await res.json();
-
+    console.log(json.pets);
     setPets(json.pets);
   }
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
