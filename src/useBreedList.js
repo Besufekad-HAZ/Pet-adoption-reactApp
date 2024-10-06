cdf36cbfc261b0dffaa4a1810003ebf1ed787989
Besufekad-HAZ <besurye@gmail.com>
2024-10-06 20:05:18 +0300

    Create a Custom hook to handle breeds fetch

---
+++ src/useBreedList.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { ANIMALS } from "./SearchParams";
+
+const LOCAL_STORAGE_KEY = "breedList";
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState(
+    localStorage.getItem(LOCAL_STORAGE_KEY) ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))[animal] : []
+  );
+
+  useEffect(() => {
+    if (animal) {
+      const fetchBreeds = async () => {
+        const res = await fetch(
+          `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+        );
+        const json = await res.json();
+        const breeds = json.breeds.map(breed => breed.name);
+        setBreedList(
+          breeds
+        );
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({ [animal]: breeds }));
+      };
+      fetchBreeds();
+    }
+  }, [animal]);
+
+  return [breedList, setBreedList];
+}
