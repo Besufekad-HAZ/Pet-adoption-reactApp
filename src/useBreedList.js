3a502bd278214b6c73826119abe14fdcb1ff590e
Besufekad-HAZ <besurye@gmail.com>
2024-10-06 20:08:23 +0300

    Add localCache into the use BreedList component

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,25 +1,27 @@
 import { useState, useEffect } from "react";
-import { ANIMALS } from "./SearchParams";
 
-const LOCAL_STORAGE_KEY = "breedList";
+const localCache = {};
 
 export default function useBreedList(animal) {
   const [breedList, setBreedList] = useState(
-    localStorage.getItem(LOCAL_STORAGE_KEY) ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))[animal] : []
+    localStorage.getItem(LOCAL_STORAGE_KEY)
+      ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))[animal]
+      : [],
   );
 
   useEffect(() => {
     if (animal) {
       const fetchBreeds = async () => {
         const res = await fetch(
-          `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+          `http://pets-v2.dev-apis.com/breeds?animal=${animal}`,
         );
         const json = await res.json();
-        const breeds = json.breeds.map(breed => breed.name);
-        setBreedList(
-          breeds
+        const breeds = json.breeds.map((breed) => breed.name);
+        setBreedList(breeds);
+        localStorage.setItem(
+          LOCAL_STORAGE_KEY,
+          JSON.stringify({ [animal]: breeds }),
         );
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({ [animal]: breeds }));
       };
       fetchBreeds();
     }
