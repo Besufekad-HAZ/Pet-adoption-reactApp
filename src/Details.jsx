69ea6cdb75efcaf46d4415f526089b4cc2e63891
Besufekad-HAZ <besurye@gmail.com>
2024-10-07 14:14:03 +0300

    Use useParams in the Details page

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { useParams } from "react-router-dom";
 
 const Details = () => {
-  return <div>Hellloooo</div>;
+  const { id } = useParams();
+
+  return <div>The Animal Id is {id}</div>;
 };
 
 export default Details;
