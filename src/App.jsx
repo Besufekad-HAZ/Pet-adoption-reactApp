844fe3d8d6443eebb3c3f620981bc04d4eba4c39
Besufekad-HAZ <besurye@gmail.com>
2024-10-07 13:51:57 +0300

    Import BrowserRouter and wrap everything inside it for use and remove the SearchParams component for use

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import SearchParams from "./SearchParams";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Adopt Me!</h1>
+      </div>
+    </BrowserRouter>
   );
 };
 
