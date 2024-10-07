b9df877254ca9d3c41b33caf015ddad33baf0a33
Besufekad-HAZ <besurye@gmail.com>
2024-10-07 13:57:10 +0300

    Add id={pet.id} in the Results component page and define the routes and router things iniside the App.jsx parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Adopt Me!</h1>
-      </div>
+      <h1>Adopt Me!</h1>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
     </BrowserRouter>
   );
 };
