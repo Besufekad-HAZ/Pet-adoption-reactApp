eb6289f152b79939562a6e374d7e0d44a40c2b01
Besufekad-HAZ <besurye@gmail.com>
2024-10-09 13:55:44 +0300

    Create Erorr Boundary

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+class ErrorBoundary extends React.Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    logErrorToServices(errorInfo.componentStack);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
