21122269edc6d7189fa80c3583f1347ba224d0ab
Besufekad-HAZ <besurye@gmail.com>
2024-10-09 13:59:15 +0300

    Add componentDidCatch in the Erorr Boundary file

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -6,6 +6,10 @@ class ErrorBoundary extends Component {
   static getDerivedStateFromError() {
     return { hasError: true };
   }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
   render() {
     if (this.state.hasError) {
       return (
