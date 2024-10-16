95bc7112461ca785943646a62b296c0c2654578a
Besufekad-HAZ <besurye@gmail.com>
2024-10-16 15:44:50 +0300

    Implement tailwind styling inside the Details and Carousel components

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -19,9 +19,13 @@ class Carousel extends Component {
     const { active } = this.state;
     const { images } = this.props;
     return (
-      <div className="carousel">
-        <img src={images[active]} alt="animal" />
-        <div className="carousel-smaller">
+      <div className="carousel mt-2 flex h-96 flex-col items-center justify-around lg:flex-row">
+        <img
+          src={images[active]}
+          alt="animal"
+          className="lg:max-w-1/2 max-h-full max-w-full"
+        />
+        <div className="carousel-smaller grid w-full grid-cols-3 lg:flex lg:w-2/4 lg:items-center lg:justify-center">
           {images.map((photo, index) => (
             // eslint-disable-next-line
             <img
@@ -29,7 +33,11 @@ class Carousel extends Component {
               data-index={index}
               key={photo}
               src={photo}
-              className={index === active ? "active" : ""}
+              className={`m-4 inline-block h-24 w-24 cursor-pointer rounded-full border-2 ${
+                index === active
+                  ? "border-gray-800 opacity-60"
+                  : "border-gray-300"
+              }`}
               alt="animal thumbnail"
             />
           ))}
--- src/Details.jsx
+++ src/Details.jsx
@@ -13,16 +13,14 @@ const Details = () => {
     queryKey: ["details", id],
     queryFn: fetchPet,
   });
-  // add showModal
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
-  // eslint-disable-next-line no-unused-vars
   const [_, setAdoptedPet] = useContext(AdoptedPetContext);
 
   if (results.isLoading) {
     return (
-      <div className="loading-pane">
-        <h2 className="loader">🌀</h2>
+      <div className="loading-pane flex h-screen items-center justify-center">
+        <h2 className="loader text-4xl">🌀</h2>
       </div>
     );
   }
@@ -30,19 +28,27 @@ const Details = () => {
   const pet = results.data.pets[0];
 
   return (
-    <div className="details">
+    <div className="container mx-auto p-4">
       <Carousel images={pet.images} />
-      <div>
-        <h1>{pet.name}</h1>
-        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
-        <p>{pet.description}</p>
+      <div className="mt-8 text-center">
+        <h1 className="my-2 text-5xl text-gray-800">{pet.name}</h1>
+        <h2 className="my-2 mb-5 text-xl">{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button
+          className="mt-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={() => setShowModal(true)}
+        >
+          Adopt {pet.name}
+        </button>
+        <p className="px-4 leading-6">{pet.description}</p>
         {showModal ? (
           <Modal>
-            <div>
-              <h1>Would you like to adopt {pet.name}?</h1>
-              <div className="buttons">
+            <div className="rounded bg-white p-8 text-center shadow-lg">
+              <h1 className="mb-4 text-2xl">
+                Would you like to adopt {pet.name}?
+              </h1>
+              <div className="buttons flex justify-center space-x-4">
                 <button
+                  className="rounded bg-green-500 px-4 py-2 text-white hover:bg-green-700"
                   onClick={() => {
                     setAdoptedPet(pet);
                     navigate("/");
@@ -50,7 +56,12 @@ const Details = () => {
                 >
                   Yes
                 </button>
-                <button onClick={() => setShowModal(false)}>No</button>
+                <button
+                  className="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-700"
+                  onClick={() => setShowModal(false)}
+                >
+                  No
+                </button>
               </div>
             </div>
           </Modal>
