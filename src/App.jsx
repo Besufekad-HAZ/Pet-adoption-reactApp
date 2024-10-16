7251ce6080aeab614ad64199cbdbff205f004718
Besufekad-HAZ <besurye@gmail.com>
2024-10-16 22:24:39 +0300

    Remove the react dom related things from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, lazy, Suspense } from "react";
-import { createRoot } from "react-dom/client";
 import { Route, Routes, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
