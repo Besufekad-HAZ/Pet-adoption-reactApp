eafc965714e3433104503c10588d2a4ccd0ee578
Besufekad-HAZ <besurye@gmail.com>
2024-10-22 12:19:23 +0300

    Remove the useState and Context from App component to later use RTK

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
-import AdoptedPetContext from "./AdoptedPetContext";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 import { Pet } from "./APIResponsesTypes";
