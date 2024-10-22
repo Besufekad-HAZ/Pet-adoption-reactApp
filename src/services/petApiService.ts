44c984b2533ad6fe8a527d6b3f9740e647b922f2
Besufekad-HAZ <besurye@gmail.com>
2024-10-22 14:15:42 +0300

    Create an API service using Redux Toolkit Query to fetch pet details by ID and provided a hook (useGetPetQuery)

--- src/services/petApiService.ts
+++ src/services/petApiService.ts
@@ -1,11 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { PetAPIResponse } from "../APIResponsesTypes";
 
 export const petApi = createApi({
   reducerPath: "petApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://pets-v2.dev-apis.com" }),
   endpoints: (builder) => ({
-    listPets: builder.query({
-      query: () => "pets",
+    getPet: builder.query({
+      query: (id: string) => ({
+        url: "pets",
+        params: {
+          id,
+        },
+      }),
+      transformResponse: (response: PetAPIResponse) => response.pets[0],
     }),
   }),
 });
+
+export const { useGetPetQuery } = petApi;
