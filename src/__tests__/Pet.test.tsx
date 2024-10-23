66f8a35ec0c2b6d1066c416e9585dcfddce8ea62
Besufekad-HAZ <besurye@gmail.com>
2024-10-23 12:52:32 +0300

    Use the StaticRouter in the test code

--- src/__tests__/Pet.test.tsx
+++ src/__tests__/Pet.test.tsx
@@ -4,7 +4,19 @@ import { render } from "@testing-library/react";
 import Pet from "../Pet";
 
 test("displays a default thumbnail", async () => {
-  const pet = render(<Pet name={""} animal={"dog"} breed={""} images={[]} location={""} id={0} />);
+  const pet = render(
+    <StaticRouter location="">
+      <Pet
+        name={""}
+        animal={"dog"}
+        breed={""}
+        images={[]}
+        location={""}
+        id={0}
+      />
+      ,
+    </StaticRouter>,
+  );
 
   const petThumbnail = await pet.findByTestId("thumbnail");
   expect((petThumbnail as HTMLImageElement).src).toContain("none.jpg");
