7ccb58546533bdf343b67752d3662677b0c6f3d4
Besufekad-HAZ <besurye@gmail.com>
2024-10-23 13:52:15 +0300

    Wrap the test with both QueryClientProvider and the Redux Provider and check the test is passing

--- src/__tests__/useBreedList.test.tsx
+++ src/__tests__/useBreedList.test.tsx
@@ -1,10 +1,9 @@
 import { expect, test } from "vitest";
-import { render } from "@testing-library/react";
+import { renderHook } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 import useBreedList from "../useBreedList";
-import { Animal } from "../APIResponsesTypes";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,26 +14,19 @@ const queryClient = new QueryClient({
   },
 });
 
-function getBreedList(animal: Animal) {
-  let list: [string[], string] | undefined;
-
-  function TestComponent() {
-    list = useBreedList(animal) as [string[], string];
-    return null;
-  }
-
-  render(
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <TestComponent />
-      </QueryClientProvider>
-    </Provider>,
-  );
-
-  return list;
-}
-
 test("gives an empty list with no animal", () => {
-  const list = getBreedList("" as Animal);
-  expect(list).toEqual([[], "loading"]);
+  const { result } = renderHook(() => useBreedList("dog"), {
+    wrapper: ({ children }) => (
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </Provider>
+    ),
+  });
+
+  const [breedList, status] = result.current;
+
+  expect(breedList).toHaveLength(0);
+  expect(status).toBe("loading");
 });
