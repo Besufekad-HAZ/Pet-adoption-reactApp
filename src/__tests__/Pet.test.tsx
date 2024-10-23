ba7ae37cf940f812b495f244b0a443f9c77dfdd4
Besufekad-HAZ <besurye@gmail.com>
2024-10-23 12:55:21 +0300

    Write a second test to check the non default case

--- src/__tests__/Pet.test.tsx
+++ src/__tests__/Pet.test.tsx
@@ -22,3 +22,15 @@ test("displays a default thumbnail", async () => {
   expect((petThumbnail as HTMLImageElement).src).toContain("none.jpg");
   pet.unmount();
 });
+
+test("displays a non-default thumbnail", async () => {
+  const pet = render(
+    <StaticRouter location="">
+      <Pet images={["1.jpg", "2.jpg", "3.jpg"]} name={""} animal={"dog"} breed={""} location={""} id={0} />
+    </StaticRouter>,
+  );
+
+  const petThumbnail = await pet.findByTestId("thumbnail");
+  expect(petThumbnail.src).toContain("1.jpg");
+  pet.unmount();
+});
